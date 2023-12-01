d9cb08e41b6873a713927fbffa1a0388c73a8af6
rmartinperea <rmartinp.it@gmail.com>
2023-12-01 18:36:09 +0100

    actualizado

--- main.js
+++ main.js
@@ -1,43 +1,93 @@
-//Practica 1
+//descendiente de main.js
 
-let msg = "hola mundo"
-
-console.log(msg)
-
-document.getElementById("title")
-
-/* document.addEventListener se utiliza para
-  agregar un "escuchador de eventos al documento HTML" */
- /*"DOMContentLoaded" significa que queremos hacer algo cuando el contenido
-  del DOM (Document Object Model) haya cargado completamente. */
-document.addEventListener("DOMContentLoaded", init)
+/*IMPORT DE TODO
+import * as p1 from "./practica1.js"*/ 
 
 
+import {secundaria} from "./practica1.js"
 
-function init(event) {
-    let title = document.getElementById("title")
-    title.style.color = "red"
-    title.value = msg
-    loadData("data.json")
+let principal = 100
+
+//FUNCION
+
+//para hacerlo funcion hacen falta los ()
+/*resultado es un objeto
+innerHTML es lo que hay dentro del <div>hola</div> del html.
+*/
+function saludar(nombre){
+    let resultado = document.getElementById("resultado")
+    resultado.innerHTML = nombre
 }
 
-/*    fetch(url).then
-        .then(Response => json = Response.json())
-        .then(data => {
-            json = data
-            console.log(json)
-        }) */
-
-function loadData(url){
-fetch(url)
-
-.then(response => response.json())
-.then(data => pintaMisHobbies(data))
+//estructuras de control
+let ficha = {
+nombre: "ruben",
+personaFavorita: "cualquier perro",
+ponerNota: calcularNota
+}
+function calcularNota() {
+    //todo lo que sea
+    return 0;
 }
 
-function pintaMisHobbies(json){
-    console.log(json)
+//operador igual es ==
+console.log("10 es igual a 20: ", 10 == 20)
+console.log("20 es igual a 20: ", 20 == 20)
+
+//operador distinto de es !=
+console.log("10 es igual a 20: ", 10 != 20)
+console.log("20 es igual a 20: ", 20 != 20)
+
+function factorial(numero) {
+    if(condition) {
+
+        factorial(numero-1)
+    }
+    return 1;
+}
+let valor = 0
+let resultado = factorial(valor)
+
+
+function factorial(numero) {
+    if(numero >= 1) {
+
+        return numero * factorial(numero-1)
+ }
+    return 1;
+}
+let valor = 10
+let resultado = factorial(valor)
+console.log("el resultado")
+
+function factorial(n)
+let m = n;
+let resultado = n;
+while (m >= 1) {
+
+return resultado;
+  }
+
+
+/*SUMA*/ 
+function suma(lista) {
+    resultado= 0;
+
+
+    
+
+    return resultado;
 }
 
+let numeros = [120, 200, 5, 48, 190, -49];
+console.log("la suma de la lista es: ", suma(numeros))
 
 
+//----------------------------------------
+//document selecciona algo del documento
+//document.getElementById("resultado")
+//para visualizarlo
+//let resultado = document.getElementById("resultado")
+
+
+/*console.log(secundaria)*/
---
+++ practica1.js
@@ -0,0 +1,8 @@
+
+/*EXPORTAR LINEA A LINEA
+export let secundaria = 50*/
+
+export let secundaria = 50
+
+
+/*export * from */
